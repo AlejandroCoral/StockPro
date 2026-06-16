28d1eb22a72b6a835f7da9ed7b25e6666dc5897b
AlejoGit <coral.alejandro05@gmail.com>
2026-06-16 16:49:39 -0500

    Implementar navegacion principal con NavHost

--- app/src/main/java/com/example/stockpro/MainActivity.kt
+++ app/src/main/java/com/example/stockpro/MainActivity.kt
@@ -5,25 +5,31 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.stockpro.ui.theme.StockProTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             StockProTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    StockProApp()
                 }
             }
         }
@@ -31,17 +37,89 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun StockProApp() {
+
+    val navController = rememberNavController()
+
+    val stockViewModel: StockViewModel = viewModel()
+
+    NavHost(
+        navController = navController,
+        startDestination = "ingreso"
+    ) {
+        composable("ingreso") {
+            PantallaIngreso(navController = navController)
+        }
+
+        composable(
+            route = "catalogo/{nombreOperario}",
+            arguments = listOf(
+                navArgument("nombreOperario") {
+                    type = NavType.StringType
+                }
+            )
+        ) { backStackEntry ->
+
+            val nombreOperario = backStackEntry.arguments?.getString("nombreOperario") ?: ""
+
+            PantallaCatalogoInventario(
+                navController = navController,
+                viewModel = stockViewModel,
+                nombreOperario = nombreOperario
+            )
+        }
+
+        composable(
+            route = "editar/{productoId}",
+            arguments = listOf(
+                navArgument("productoId") {
+                    type = NavType.IntType
+                }
+            )
+        ) { backStackEntry ->
+
+            val productoId = backStackEntry.arguments?.getInt("productoId") ?: 0
+
+            PantallaEditarStock(
+                navController = navController,
+                viewModel = stockViewModel,
+                productoId = productoId
+            )
+        }
+
+        composable("reporte") {
+            PantallaReporteFinanciero(
+                navController = navController,
+                viewModel = stockViewModel
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    StockProTheme {
-        Greeting("Android")
-    }
+fun PantallaReporteFinanciero(navController: NavHostController, viewModel: StockViewModel) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun PantallaEditarStock(
+    navController: NavHostController,
+    viewModel: StockViewModel,
+    productoId: Int
+) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun PantallaCatalogoInventario(
+    navController: NavHostController,
+    viewModel: StockViewModel,
+    nombreOperario: String
+) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun PantallaIngreso(navController: NavHostController) {
+    TODO("Not yet implemented")
 }
